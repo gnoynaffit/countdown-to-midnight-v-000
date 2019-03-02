b5b575d7b366ac0ecc49f5cbde37aeeb59578c49
Tiffany <ong.tifffany1503@gmail.com>
2019-03-02 18:41:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,3 +5,14 @@ while num > 0
   puts "#{number} SECOND(S)!"
   num -= 1
 end
+ "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  while num > 0
+    puts "#{number} SECOND(S) !"
+    sleep (1)
+    num -=  1
+  end
+  "HAPPY NEW YEAR!"
+end
